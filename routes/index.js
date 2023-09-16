26723b1952643b7dfffba51ada7288d7ae23cbde
Charu Patel <90954539+Sunshadeshine@users.noreply.github.com>
2023-09-16 23:50:44 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ import authRoutes from "./auth.js";
 import categoryRoute from "./category.js";
 import productRoute from "./product.js";
 import path from "path";
-
+import {fileURLToPath} from 'url';
+const __filename = fileURLToPath(import.meta.url); 
+const __dirname=path.dirname(__filename);
 console.log("router loaded");
 import {fileURLToPath} from 'url';
 
