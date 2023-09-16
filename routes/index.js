dcc8d70c321c4986d958945cf4b387ab42c95b03
Charu Patel <90954539+Sunshadeshine@users.noreply.github.com>
2023-09-16 23:12:31 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,13 @@ import * as homeController from "../controllers/home-controller.js";
 import authRoutes from "./auth.js";
 import categoryRoute from "./category.js";
 import productRoute from "./product.js";
-
+import path from "path";
 const router = express.Router();
 console.log("router loaded");
 
-router.get("/", homeController.home);
+router.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 router.use("/api/v1/auth", authRoutes);
 router.use("/api/v1/category", categoryRoute);
