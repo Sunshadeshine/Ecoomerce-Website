4e7f994ab2772fe4915ac7b82844156483059696
Charu Patel <90954539+Sunshadeshine@users.noreply.github.com>
2023-09-17 11:50:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const __dirname=path.dirname(__filename);
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "./client")));
 
 // const db = require('./config/mongoose');
 app.use("/", routes);
